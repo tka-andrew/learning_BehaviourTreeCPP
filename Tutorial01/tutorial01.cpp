3a0f3af08c77ef684e5375dee7b648eb30852441
tka-andrew <kui.an.andrew.tan@gmail.com>
2022-04-29 22:41:19 +0800

    minimal example of including behaviourTree library

--- Tutorial01/tutorial01.cpp
+++ Tutorial01/tutorial01.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include "behaviortree_cpp_v3/bt_factory.h"
 
 int main() {
+    // We use the BehaviorTreeFactory to register our custom nodes
+    BT::BehaviorTreeFactory factory;
+
     std::cout<<"This is tutorial 01.\n";
 }
