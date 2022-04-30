d16ae8138f25c4a27371d7473121eb62b0063b6a
tka-andrew <kui.an.andrew.tan@gmail.com>
2022-04-30 14:49:31 +0800

    done tutorial01

--- Tutorial01/tutorial01.cpp
+++ Tutorial01/tutorial01.cpp
@@ -1,9 +1,51 @@
-#include <iostream>
 #include "behaviortree_cpp_v3/bt_factory.h"
 
-int main() {
+// file that contains the custom nodes definitions
+#include "dummy_nodes.h"
+
+#include <iostream>
+
+int main()
+{
+    std::cout<<"============ Tutorial 01 ============\n";
+
     // We use the BehaviorTreeFactory to register our custom nodes
     BT::BehaviorTreeFactory factory;
 
-    std::cout<<"This is tutorial 01.\n";
+    // The recommended way to create a Node is through inheritance.
+    factory.registerNodeType<DummyNodes::ApproachObject>("ApproachObject");
+
+    // Registering a SimpleActionNode using a function pointer.
+    // you may also use C++11 lambdas instead of std::bind
+    factory.registerSimpleCondition("CheckBattery", std::bind(DummyNodes::CheckBattery));
+
+    //You can also create SimpleActionNodes using methods of a class
+    DummyNodes::GripperInterface gripper;
+    factory.registerSimpleAction("OpenGripper", 
+                                 std::bind(&DummyNodes::GripperInterface::open, &gripper));
+    factory.registerSimpleAction("CloseGripper", 
+                                 std::bind(&DummyNodes::GripperInterface::close, &gripper));
+
+    // Trees are created at deployment-time (i.e. at run-time, but only 
+    // once at the beginning). 
+
+    // IMPORTANT: when the object "tree" goes out of scope, all the 
+    // TreeNodes are destroyed
+    auto tree = factory.createTreeFromFile("./my_tree.xml");
+
+    // To "execute" a Tree you need to "tick" it.
+    // The tick is propagated to the children based on the logic of the tree.
+    // In this case, the entire sequence is executed, because all the children
+    // of the Sequence return SUCCESS.
+    tree.tickRoot();
+
+    return 0;
 }
+
+/* Expected output:
+*
+       [ Battery: OK ]
+       GripperInterface::open
+       ApproachObject: approach_object
+       GripperInterface::close
+*/
