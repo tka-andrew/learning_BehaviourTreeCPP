36df9233942473fc5a0767d87376e4d0511dc272
tka-andrew <kui.an.andrew.tan@gmail.com>
2022-05-03 12:37:45 +0800

    removed unnecessary include

--- Tutorial03/tutorial03_interface.h
+++ Tutorial03/tutorial03_interface.h
@@ -1,17 +1,13 @@
 #ifndef T03_INTERFACE_H
 #define T03_INTERFACE_H
 
-#include "behaviortree_cpp_v3/bt_factory.h"
-
 namespace T03Interface
 {
-    // We want to be able to use this custom type
     struct Position2D
     {
         double x;
         double y;
     };
-
 }
 
 #endif // T03_INTERFACE_H
